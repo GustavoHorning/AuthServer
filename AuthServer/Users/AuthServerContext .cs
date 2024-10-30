8d310d149d48db1043de29dd1bcf47925fc2bf81
Gustavo Horning <121242572+GustavoHorning@users.noreply.github.com>
2024-10-30 13:45:16 -0300

    commit 1 - ajuste

---
+++ AuthServer/Users/AuthServerContext .cs
@@ -0,0 +1,26 @@
+﻿using AuthServer.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer
+{
+    public class AuthServerContext : DbContext
+    {
+        public AuthServerContext(DbContextOptions<AuthServerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } // DbSet para a entidade User
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Mapeando a classe User para a tabela "Users"
+            modelBuilder.Entity<User>().ToTable("Users");
+
+            // Configurando o índice único para o campo Email
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique(); // Define que o índice deve ser único
+        }
+    }
+}
